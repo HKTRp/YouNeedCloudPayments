0986a7eb006d692caa9fbbbfa9aaf65bada3a0fd
HKTRp <danilburzua@mail.ru>
2022-11-10 09:57:33 +0300

    Server https test

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('ssl/key.pem'),
+    cert: fs.readFileSync('ssl/cert.pem')
+};
 
 const host = '195.43.142.88';
 const port = 80;
@@ -9,7 +16,7 @@ const requestListener = function (req, res) {
     res.end("My first server!");
 };
 
-const server = http.createServer(requestListener);
+const server = https.createServer(options, requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Server is running on https://${host}:${port}`);
 });
